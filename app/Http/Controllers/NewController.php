f690dd19b1e83eb084b4ebe2be989cc17fb1d502
Pzulhisyam <zulhisyammmmm@gmail.com>
2021-01-15 10:17:34 +0800

    first commit

---
+++ app/Http/Controllers/NewController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewController extends Controller
+{
+    public function index(){
+		return view('welcome');
+	}
+
+	public function packageFunctions(){
+		echo "hello world!";
+		return view('package1.newPackage');
+	}
+}
